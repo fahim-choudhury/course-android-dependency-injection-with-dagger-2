a6740e0ea5580441c28c20faa06efcc39061941f
Fahim Masud Choudhury <fahim.m.choudhury@gmail.com>
2024-08-30 12:52:31 +0600

    Make PresentationComponent depend on ActivityComponent

--- app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/presentation/PresentationComponent.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/presentation/PresentationComponent.kt
@@ -1,15 +1,12 @@
 package com.techyourchance.dagger2course.common.dependnecyinjection.presentation
 
-import com.techyourchance.dagger2course.questions.FetchQuestionDetailsUseCase
-import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
-import com.techyourchance.dagger2course.screens.common.ScreensNavigator
-import com.techyourchance.dagger2course.screens.common.dialogs.DialogsNavigator
-import com.techyourchance.dagger2course.screens.common.viewsmvc.ViewMvcFactory
+import com.techyourchance.dagger2course.common.dependnecyinjection.activity.ActivityComponent
 import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsActivity
 import com.techyourchance.dagger2course.screens.questionslist.QuestionsListFragment
 import dagger.Component
 
-@Component(modules = [PresentationModule::class])
+@Component(dependencies = [ActivityComponent::class], modules = [PresentationModule::class])
+@PresentationScope
 interface PresentationComponent {
     fun inject(target: QuestionDetailsActivity)
     fun inject(target: QuestionsListFragment)
