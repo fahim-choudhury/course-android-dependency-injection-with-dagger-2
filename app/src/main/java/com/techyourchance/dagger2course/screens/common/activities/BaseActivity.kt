3ba93ae0bbf99982e8f58b60403a3a14440c424a
Fahim Masud Choudhury <fahim.m.choudhury@gmail.com>
2024-08-30 12:07:08 +0600

    Move DI classes into different packages

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/activities/BaseActivity.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/activities/BaseActivity.kt
@@ -2,11 +2,11 @@ package com.techyourchance.dagger2course.screens.common.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import com.techyourchance.dagger2course.MyApplication
-import com.techyourchance.dagger2course.common.dependnecyinjection.ActivityModule
-import com.techyourchance.dagger2course.common.dependnecyinjection.DaggerActivityComponent
-import com.techyourchance.dagger2course.common.dependnecyinjection.DaggerPresentationComponent
+import com.techyourchance.dagger2course.common.dependnecyinjection.activity.ActivityModule
 import com.techyourchance.dagger2course.common.dependnecyinjection.Injector
-import com.techyourchance.dagger2course.common.dependnecyinjection.PresentationModule
+import com.techyourchance.dagger2course.common.dependnecyinjection.activity.DaggerActivityComponent
+import com.techyourchance.dagger2course.common.dependnecyinjection.presentation.DaggerPresentationComponent
+import com.techyourchance.dagger2course.common.dependnecyinjection.presentation.PresentationModule
 
 open class BaseActivity: AppCompatActivity() {
 
