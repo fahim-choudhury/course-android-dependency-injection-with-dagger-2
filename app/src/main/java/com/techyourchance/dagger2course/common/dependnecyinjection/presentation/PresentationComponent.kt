a1ee0057687cac6fdfb5e0982160502eb4424d95
Fahim Masud Choudhury <fahim.m.choudhury@gmail.com>
2024-08-30 12:38:25 +0600

    Remove Injector and use PresentationComponent as injector

--- app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/presentation/PresentationComponent.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/presentation/PresentationComponent.kt
@@ -5,19 +5,13 @@ import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
 import com.techyourchance.dagger2course.screens.common.ScreensNavigator
 import com.techyourchance.dagger2course.screens.common.dialogs.DialogsNavigator
 import com.techyourchance.dagger2course.screens.common.viewsmvc.ViewMvcFactory
+import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsActivity
+import com.techyourchance.dagger2course.screens.questionslist.QuestionsListFragment
 import dagger.Component
 
 @Component(modules = [PresentationModule::class])
 interface PresentationComponent {
-
-    fun viewMvcFactory(): ViewMvcFactory
-
-    fun screensNavigator(): ScreensNavigator
-
-    fun dialogsNavigator(): DialogsNavigator
-
-    fun fetchQuestionsUseCase(): FetchQuestionsUseCase
-
-    fun fetchQuestionDetailsUseCase(): FetchQuestionDetailsUseCase
+    fun inject(target: QuestionDetailsActivity)
+    fun inject(target: QuestionsListFragment)
 
 }
--- app/src/main/java/com/techyourchance/dagger2course/screens/common/activities/BaseActivity.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/activities/BaseActivity.kt
@@ -3,7 +3,6 @@ package com.techyourchance.dagger2course.screens.common.activities
 import androidx.appcompat.app.AppCompatActivity
 import com.techyourchance.dagger2course.MyApplication
 import com.techyourchance.dagger2course.common.dependnecyinjection.activity.ActivityModule
-import com.techyourchance.dagger2course.common.dependnecyinjection.Injector
 import com.techyourchance.dagger2course.common.dependnecyinjection.activity.DaggerActivityComponent
 import com.techyourchance.dagger2course.common.dependnecyinjection.presentation.DaggerPresentationComponent
 import com.techyourchance.dagger2course.common.dependnecyinjection.presentation.PresentationModule
@@ -22,5 +21,5 @@ open class BaseActivity: AppCompatActivity() {
                 .build()
     }
 
-    protected val injector get() = Injector(presentationComponent)
+    protected val injector get() = presentationComponent
 }
